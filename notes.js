2c0f58a0e32aaeb1926ceeb69652169b500ad579
Jashele Tillman <48870048+jasheloper@users.noreply.github.com>
2019-12-14 14:21:16 -0700

    there is now a test file for the list of fav apps api

--- notes.js
+++ notes.js
@@ -2,26 +2,36 @@
 
 //////////////////////////////////////////////////////////
 
-/*  What will be the resource?  A list of apps.
-   Name:    +     Description:  
+/*  What will be the resource?  
+   A list of my favorite apps.
+
+   {
+      "Name:" ______ ,
+      "Description:" ________ 
+   }
        
 */
 
+
    
-/*     ***   MVP:  ***
-      2 tests per route, written before production code
+/*    MVP: 
+      * 2 tests per route, written before production code
 
-      Route Handlers - GET  | CREATE  | DELETE
+      * Route Handlers - GET  | CREATE  | DELETE
 
-      Storage? Token. 
+      * Storage? Token. 
 
-      Database? Nah. maybe later.  */
+      * Database? Nah. maybe later.  */
 
 //////////////////////////////////////////////////////////
 
 
 /* Documentation of my Process for this
 
+1. Write the tests.
+
+   * Lecture Review:    https://youtu.be/zgrceUO_2z8?t=1186
+   *  Create a folder for tests > [touch] apps.test.js  
 
 
 */
